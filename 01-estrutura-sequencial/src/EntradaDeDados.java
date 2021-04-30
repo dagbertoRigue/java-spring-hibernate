f2b4d43b00f2d3d337a1accc8b41a53658c59f51
Dagberto Rigue <riguealive@gmail.com>
2021-04-30 02:10:55 -0300

    Add files via upload

--- 01-estrutura-sequencial/src/EntradaDeDados.java
+++ 01-estrutura-sequencial/src/EntradaDeDados.java
@@ -23,8 +23,32 @@ public class EntradaDeDados {
 		
 		char w;
 		w = sc.next().charAt(0);
-		System.out.println("Char : " + w);		
+		System.out.println("Char : " + w);
 		
+		System.out.println();
+		System.out.println("------------------------------------------------------------------------------------------");
+		System.out.println("Entrada de dados com quebra de linha. Pode-se salvar na variável a informação digitada até "
+				+ "a quebra de linha.");
+		System.out.println("Para testar, digite um pequeno texto, aperte o enter e repita isso mais duas vezes..");
+		
+		String s1, s2, s3, s4;
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		s4 = sc.nextLine();
+		
+		System.out.println("Você digitou :");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);	
+		System.out.println(s4);
+		
+		System.out.println("Obs. nesse exemplo, está sendo mostrado apenas as variáveis s2 a s4.");
+		System.out.println("Isso acontece porque é digitado enter no exemplo anterior.");
+		System.out.println("Nesse caso, o s1 recebe o enter e salva uma informação null.");
+		System.out.println("Isso se chama quebra de linha pendente");
+		System.out.println("Para resolver isso, adiciona-se um sc.nextLine() antes do s1");
+				
 		sc.close();
 	}
 
