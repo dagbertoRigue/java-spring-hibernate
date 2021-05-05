971e106ceb1c06bdd66d6ac2a65e4ac64aad88fb
Dagberto Rigue <riguealive@gmail.com>
2021-05-05 14:18:42 -0300

    Add files via upload

--- 03-estruturas-repetitivas/src/EstruturaRepetitivaFor.java
+++ 03-estruturas-repetitivas/src/EstruturaRepetitivaFor.java
@@ -1,17 +1,49 @@
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class EstruturaRepetitivaFor {
 
 	public static void main(String[] args) {
 		System.out.println("---------------------------------------------------------------------------------------------");
 		System.out.println("For - estrutura repetitiva de controle que repete um bloco de comandos para um certo intervalo de valores.");
-		System.out.println("Quando usar : quando se sabe previamente a quantidade de repetições, ou o intervalo de valores.\n");
+		System.out.println("Quando usar : quando se sabe previamente a quantidade de repetições, ou o intervalo de valores.\n");		
 		
-		System.out.println("Exemplo : Fazer um programa que lê um valor inteiro N e depois N números inteiros."
-				+ "Ao final, mostra a soma dos N números lidos");
-		System.out.println("Sintaxe : for (início; condição; incremento) { \n"
+		System.out.println("Sintaxe : \n for (início; condição; incremento) { \n"
 				+ "comando 1 \n"
-				+ "comando 2 \n");
+				+ "comando 2 \n"
+				+ "}");
+		System.out.println("O início executa apenas uma vez. A condição, se verdadeira executa e volta. Se for falsa, pula fora. "
+				+ "O incremento executa toda vez depois de voltar. \n");
+		
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("Exemplo 1 : Implementando um contador progressivo. \n");
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("Valor de i : " + i);
+		}
 
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("Exemplo 2 : Implementando um contador regressivo. \n");
+		
+		for (int i=4; i>=0; i--) {
+			System.out.println("Valor de i : " + i);
+		}
+		
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("Exemplo 3 : Fazer um programa que lê um valor inteiro N e depois N números inteiros. \n"
+				+ "Ao final, mostra a soma dos N números lidos. \n");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe um número \"N\" : ");
+		int N = sc.nextInt();		
+		int soma = 0;
+		
+		for(int i=0; i<N; i++) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+		System.out.println("Soma = " + soma);
+		sc.close();
 	}
 
 }
--- 03-estruturas-repetitivas/src/Exemplos.java
+++ 03-estruturas-repetitivas/src/Exemplos.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Exemplos {
 
-	public static void main(String[] args) {/*
+	public static void main(String[] args) {
 		System.out.println("-----------------------------------------------------------------------------------------------------------------------");
 		System.out.println("Exemplo 1 : Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha\n"
 				+ "incorreta informada, escrever a mensagem \"Senha Invalida\". Quando a senha for informada corretamente deve ser\n"
 				+ "impressa a mensagem \"Acesso Permitido\" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.");
-		*/
-		Scanner sc = new Scanner(System.in);/*
+		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Informe a senha : ");
 		int senha = sc.nextInt();
 		
@@ -42,7 +42,7 @@ public class Exemplos {
 			coordy = sc.nextInt();
 		}
 		System.out.println("Fim do programa !");
-	*/
+	
 		System.out.println("-----------------------------------------------------------------------------------------------------------------------");
 		System.out.println("Exemplo 3 : Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes.\n"
 				+ "Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel\n"
