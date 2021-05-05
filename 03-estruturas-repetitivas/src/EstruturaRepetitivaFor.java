349b8851d7112fde24fb47341a9815f5f3d0ce93
Dagberto Rigue <riguealive@gmail.com>
2021-05-05 13:26:12 -0300

    Add files via upload

---
+++ 03-estruturas-repetitivas/src/EstruturaRepetitivaFor.java
@@ -0,0 +1,17 @@
+
+public class EstruturaRepetitivaFor {
+
+	public static void main(String[] args) {
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("For - estrutura repetitiva de controle que repete um bloco de comandos para um certo intervalo de valores.");
+		System.out.println("Quando usar : quando se sabe previamente a quantidade de repetições, ou o intervalo de valores.\n");
+		
+		System.out.println("Exemplo : Fazer um programa que lê um valor inteiro N e depois N números inteiros."
+				+ "Ao final, mostra a soma dos N números lidos");
+		System.out.println("Sintaxe : for (início; condição; incremento) { \n"
+				+ "comando 1 \n"
+				+ "comando 2 \n");
+
+	}
+
+}
