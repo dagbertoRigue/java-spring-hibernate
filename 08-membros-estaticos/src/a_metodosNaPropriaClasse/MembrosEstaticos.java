31845b8451094bbb9205c00642d926100fedf531
Dagberto Rigue <riguealive@gmail.com>
2021-05-13 09:32:29 -0300

    Add files via upload

--- 08-membros-estaticos/src/a_metodosNaPropriaClasse/MembrosEstaticos.java
+++ 08-membros-estaticos/src/a_metodosNaPropriaClasse/MembrosEstaticos.java
@@ -1,5 +1,35 @@
 package a_metodosNaPropriaClasse;
 
+/*
+ * Membros estáticos :
+ * Também chamados membros de classe, em oposição a membros e instância.
+ * São membros que fazem sentido independentemente de objetos. 
+ * Não precisam de objeto para serem chamados. São chamados a partir do próprio nome da classe.
+ * Aplicações comuns:
+ *  Classes utilitárias (Math.sqrt(double)
+ *  Declaração de constantes
+ * Uma classe que possui somente membros estáticos, pode ser uma classe estática também. 
+ * Esta classe não poderá ser instanciada.
+ * 
+ * 
+ * Problema exemplo :
+ *  Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto 
+ *  seria o valor de uma circunferência e do volume de uma esfera para um raio 
+ *  daquele valor. Informar também o valor de PI com duas casas decimais.
+ *  
+ *  Exemplo : 
+ *   Enter radius: 3.0
+ *   Circumference: 18.85
+ *   Volume: 113.10
+ *   PI value: 3.14
+ *   
+ *   Versão 1: métodos na própria classe do programa. 
+ *     Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe.
+ *   Versão 2: classe Calculator com membros de instância.
+ *   Versão 3: classe Calculator com método estático.
+ *   
+ * */
+
 public class MembrosEstaticos {
 
 	public static void main(String[] args) {
