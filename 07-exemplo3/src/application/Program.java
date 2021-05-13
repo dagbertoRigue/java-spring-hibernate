8dbe9eaca8986dfe1fc5e06ab1bda54f7475d7e2
Dagberto Rigue <riguealive@gmail.com>
2021-05-13 03:37:35 -0300

    Add files via upload

--- 07-exemplo3/src/application/Program.java
+++ 07-exemplo3/src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
 /*
  * Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres 
  * do ano (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). 
@@ -14,6 +19,30 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Student stu = new Student();
+		
+		System.out.println("Informe o nome do estudante : ");
+		stu.name = sc.nextLine();
+		System.out.println("Informe a nota do primeiro trimestre :");
+		stu.nota1Trim = sc.nextDouble();
+		System.out.println("Informe a nota do segundo trimestre :");
+		stu.nota2Trim = sc.nextDouble();
+		System.out.println("Informe a nota do terceiro trimestre :");
+		stu.nota3Trim = sc.nextDouble();
+		
+		System.out.println(stu.toString());
+		
+		if(stu.notaFinal() < 60) {
+			System.out.printf("Failed \n Faltam : %.1f pontos.", stu.pontos());
+		}
+		else {
+			System.out.println("Pass");
+		}
+		
+		sc.close();
 	}
 
 }
--- 07-exemplo3/src/entities/Student.java
+++ 07-exemplo3/src/entities/Student.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Student {
-
+	public String name;
+	public double nota1Trim;
+	public double nota2Trim;
+	public double nota3Trim;
+	
+	public double notaFinal() {
+		return nota1Trim + nota2Trim + nota3Trim;
+	}
+	
+	public double pontos() {
+		if(notaFinal() < 60.0) {
+			return 60 - notaFinal();
+		}
+		else {
+			return 0.0;
+		}
+	}
+	
+	public String toString() {
+		return name 
+				+ "\n" + String.format("%.1f", nota1Trim)
+				+ "\n" + String.format("%.1f", nota2Trim)
+				+ "\n" + String.format("%.1f", nota3Trim)
+				+ "\n Nota Final = " + notaFinal();
+		
+	}
 }
