fed40da602714b330920c43656c5189be9611772
Dagberto Rigue <riguealive@gmail.com>
2021-05-13 01:37:01 -0300

    Add files via upload

--- 07-exemplo2/src/application/Program.java
+++ 07-exemplo2/src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ * Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). 
+ * Em seguida, mostrar os dados do funcionário (nome e salário líquido). 
+ * Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada 
+ * (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário. 
+ * Use a classe projetada abaixo:
+ *               __________________________________
+ *              |            Employee              |
+ *              |__________________________________|
+ *              |-Width: double                    |
+ *              |-Height: double                   |
+ *              |__________________________________|
+ *              |+Area(): double                   |
+ *              |+Perimeter(): double              |
+ *              |+Diagonal(): double               |
+ *              |__________________________________|
+ * */
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
 
 	}
 
