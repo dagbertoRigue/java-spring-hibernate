ee438469402f60d2d43f7d4849c0609feca0f956
Dagberto Rigue <riguealive@gmail.com>
2021-05-13 02:45:17 -0300

    Add files via upload

--- 07-exemplo2/src/application/Program.java
+++ 07-exemplo2/src/application/Program.java
@@ -3,22 +3,35 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Employee;
+
 /*
  * Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). 
  * Em seguida, mostrar os dados do funcionário (nome e salário líquido). 
  * Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada 
  * (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário. 
  * Use a classe projetada abaixo:
- *               __________________________________
- *              |            Employee              |
- *              |__________________________________|
- *              |-Width: double                    |
- *              |-Height: double                   |
- *              |__________________________________|
- *              |+Area(): double                   |
- *              |+Perimeter(): double              |
- *              |+Diagonal(): double               |
- *              |__________________________________|
+ *               __________________________________________
+ *              |            Employee                      |
+ *              |__________________________________________|
+ *              |-Name: String                             |
+ *              |-GrossSalary: double                      |
+ *              |-Tax: double                              |
+ *              |__________________________________________|
+ *              |+NetSalary(): double                      |
+ *              |+IncreaseSalary(percentage: double): void |
+ *              |+Diagonal(): double                       |
+ *              |__________________________________________|
+ *              
+ * Teste de mesa : 
+ * 
+ * Name: Joao Silva
+ * Gross salary: 6000.00 
+ * Tax: 1000.00 
+ * Employee: Joao Silva, $ 5000.00
+ * Which percentage to increase salary? 10.0
+ * Updated data: Joao Silva, $ 5600.00
+ *              
  * */
 public class Program {
 
@@ -27,8 +40,24 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Employee emp = new Employee();
 		
+		System.out.println("Nome: ");
+		emp.name = sc.nextLine();
+		System.out.println("Salário Bruto: ");
+		emp.grossSalary = sc.nextDouble();
+		System.out.println("Imposto: ");
+		emp.tax = sc.nextDouble();
+		
+		System.out.println(emp.toString());
+		
+		System.out.println("Qual porcentagem para incrementar o salário ?");
+		double percentage = sc.nextDouble();
+		emp.increaseSalary(percentage);
+		
+		System.out.println("Atualização de salário : " + emp);
 
+		sc.close();
 	}
 
 }
