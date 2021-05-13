c21eebc9184b3a27d22130f745525b68f3be934a
Dagberto Rigue <riguealive@gmail.com>
2021-05-13 03:52:25 -0300

    Add files via upload

---
+++ 08-membros-estaticos/src/b_comMembrosDeInstancia/MembrosEstaticos.java
@@ -0,0 +1,10 @@
+package b_comMembrosDeInstancia;
+
+public class MembrosEstaticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 08-membros-estaticos/src/c_metodoEstatico/MetodoEstatico.java
@@ -0,0 +1,10 @@
+package c_metodoEstatico;
+
+public class MetodoEstatico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
