2ee0a371a94a606719fe123529b0b951252e74f9
Dagberto Rigue <riguealive@gmail.com>
2021-05-19 23:52:22 -0300

    Add files via upload

--- 10-memoria-arrays-listas/src/exemplo/Program.java
+++ 10-memoria-arrays-listas/src/exemplo/Program.java
@@ -1,10 +1,23 @@
 package exemplo;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		Product[] vect = new Product[n];
+		
+		for(int i=0; i<n; i++) {
+			sc.nextInt();
+		}
+		
+		sc.close();
 	}
 
 }
